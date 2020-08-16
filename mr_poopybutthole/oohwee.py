c77da399e9fe51042b0b27838ce038b345ea97dd
Peter Marks <eaglerock@clutch.eagleworld.net>
2020-08-16 15:58:37 -0400

    restructure project and add gitignore

--- mr_poopybutthole/oohwee.py
+++ mr_poopybutthole/oohwee.py
@@ -6,30 +6,32 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print(f'Oooh, Wee! {client.user.name} has connected to Discord!')
+    print(f"Oooh, Wee! {client.user.name} has connected to Discord!")
+
 
 @client.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send(
-        f'Oooh, Wee! {member.name} has joined the server!'
-    )
+    await member.dm_channel.send(f"Oooh, Wee! {member.name} has joined the server!")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    matches = ['ooh', 'wee']
+    matches = ["ooh", "wee"]
 
     if any(c in message.content.lower() for c in matches):
         response = "O" + "o" * random.randint(2, 15) + "h, Wee!"
         await message.channel.send(response)
+
 
 client.run(TOKEN)
