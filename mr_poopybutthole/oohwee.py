aee1fb145ba545b7dc98bd38fbce6b65bd55ca03
Peter Marks <eaglerock@clutch.eagleworld.net>
2020-08-18 00:44:24 -0400

    add more bot

--- mr_poopybutthole/oohwee.py
+++ mr_poopybutthole/oohwee.py
@@ -19,3 +19,13 @@ class Oohwee(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         self.logger.info(f"Oooh, wee! {self.bot.user.name} has connected to Discord!")
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        await member.create_dm()
+        await member.dm_channel.send(f"Oooh, Wee! {member.name} has joined the server!")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
